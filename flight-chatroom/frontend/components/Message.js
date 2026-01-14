6a886c16dc6186c1cd1452833645534d31a2c354
ASMITA <asmitapawar3113@gmail.com>
2026-01-14 20:25:27 +0530

    Restart

--- flight-chatroom/frontend/components/Message.js
+++
@@ -1,7 +0,0 @@
-export default function Message({ user, text }) {
-    return (
-        <div className="px-4 py-2 my-1 rounded-lg shadow w-fit max-w-md bg-blue-100">
-            <span className="font-semibold">{user}:</span> {text}
-        </div>
-    );
-}
--- flight-chatroom/frontend/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client';
-
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5000', {
-  transports: ['websocket'],
-});
-
-export default socket;
